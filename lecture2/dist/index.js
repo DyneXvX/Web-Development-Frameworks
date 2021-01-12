4a580a0a57a4276b416245a0b5d845a1167b577d
Justn Thoms <DyneXvX@outlook.com>
2021-01-11 19:16:17 -0500

    lecture 2 is ready

---
+++ lecture2/dist/index.js
@@ -0,0 +1,33 @@
+"use strict";
+let helloWord = 'Hello World';
+console.log(helloWord);
+//command in terminal to compile this is tsc .\index.ts
+//command to initialize a typescript project tsc --init
+//let bigInt=100n; 
+//this required us to set the "target": "ES2020", in the tsconfig.json file. bigInt doesn't work on older version.
+function add(x, y) {
+    return x + y;
+}
+// "sourceMap": true,      /* Generates corresponding '.map' file. */ <-- really helps with debugging. Leave as true.
+// tsc -w starts watching the type script files for changes.
+console.log(add(5, 9));
+function add2(a, b) {
+    return a + b;
+}
+console.log(add2('s', true));
+var color;
+(function (color) {
+    color[color["red"] = 0] = "red";
+    color[color["green"] = 1] = "green";
+    color[color["blue"] = 2] = "blue";
+})(color || (color = {}));
+let myColor = color.green;
+//class setup can be done in typeScript
+class Student {
+    constructor(firstName) {
+        this.firstName = '';
+        this.firstName = firstName;
+    }
+}
+//cntl C will kill the watching.
+//# sourceMappingURL=index.js.map
---
+++ lecture2/index.ts
@@ -0,0 +1,45 @@
+let helloWord= 'Hello World';
+console.log(helloWord);
+//command in terminal to compile this is tsc .\index.ts
+//command to initialize a typescript project tsc --init
+
+
+//let bigInt=100n; 
+//this required us to set the "target": "ES2020", in the tsconfig.json file. bigInt doesn't work on older version.
+
+function add(x:number , y:number):number
+{
+    return x + y;
+}
+
+// "sourceMap": true,      /* Generates corresponding '.map' file. */ <-- really helps with debugging. Leave as true.
+// tsc -w starts watching the type script files for changes.
+
+console.log(add(5,9));
+
+function add2(a:string , b:boolean)
+{
+    return a + b;
+}
+
+console.log(add2('s', true))
+
+enum color{
+    red,
+    green,
+    blue
+}
+
+let myColor = color.green;
+
+//class setup can be done in typeScript
+class Student{
+    firstName:String='';
+
+    constructor(firstName:string)
+    {
+        this.firstName = firstName;
+    }
+}
+
+//cntl C will kill the watching.
