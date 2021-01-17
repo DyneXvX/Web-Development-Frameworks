b8fdad64dedd89be5698e0e3997b8286e546fc6f
Justn Thoms <DyneXvX@outlook.com>
2021-01-16 19:19:28 -0500

    working on average in javascript with push/pop

--- Review Page/dist/index.js
+++ Review Page/dist/index.js
@@ -5,11 +5,34 @@ var userName = document.getElementById('name');
 var userCommit = document.getElementById('commit');
 var template = document.getElementById('responseTemplate');
 var insertionPoint = document.getElementById('responsePoint');
+var count = 1;
+var average = [];
 btnSubmit.addEventListener('click', function (event) {
-    console.log('Rating ' + userRating.value);
-    console.log('Name ' + userName.value);
-    console.log('Commit ' + userCommit.value);
     var template = getTemplateClone('responseTemplate');
+    template.content.getElementById('userRating').innerHTML = userRating.value;
+    count = count + 1;
+    var rate = parseInt(userRating.value);
+    average.push(rate);
+    getAverage();
+    template.content.getElementById('userName').innerHTML = userName.value;
+    template.content.getElementById('userReview').innerText = userCommit.value;
     insertionPoint === null || insertionPoint === void 0 ? void 0 : insertionPoint.appendChild(template.content);
 });
+var sum = 5;
+function getAverage() {
+    average.forEach(function (element) {
+        sum += element;
+        console.log('Element is ' + element);
+        console.log('Sum is ' + sum);
+        average.pop();
+    });
+    console.log('the count is listed as ' + count);
+    var whatever = sum / count;
+    console.log('average is listed as ' + whatever);
+    return document.getElementById('averageRating').innerHTML = whatever.toString();
+}
+function getTemplateClone(templateID) {
+    var _a;
+    return (_a = document.getElementById('responseTemplate')) === null || _a === void 0 ? void 0 : _a.cloneNode(true);
+}
 //# sourceMappingURL=index.js.map
--- Review Page/src/index.ts
+++ Review Page/src/index.ts
@@ -1,19 +1,46 @@
 var btnSubmit = document.getElementById('submit') as HTMLButtonElement;
 
-var userRating = document.getElementById('rating') as HTMLInputElement;
+var userRating = document.getElementById('rating')as HTMLInputElement;
 var userName = document.getElementById('name') as HTMLInputElement;
 var userCommit = document.getElementById('commit') as HTMLInputElement;
 
 var template = document.getElementById('responseTemplate') as HTMLTemplateElement;
 var insertionPoint = document.getElementById('responsePoint');
 
-btnSubmit.addEventListener('click', (event)=> {
-    console.log('Rating ' + userRating.value);
-    console.log('Name ' + userName.value);
-    console.log('Commit ' + userCommit.value);
+var count = 1;
+var average: number[] = [];
 
-    var template = getTemplateClone('responseTemplate');      
+btnSubmit.addEventListener('click', (event)=> {
+    var template = getTemplateClone('responseTemplate');       
+    template.content.getElementById('userRating')!.innerHTML = userRating.value;       
+    
+    count = count + 1;      
+    var rate = parseInt(userRating.value);
+    average.push(rate);
+    getAverage();
+    template.content.getElementById('userName')!.innerHTML = userName.value;  
+    template.content.getElementById('userReview')!.innerText = userCommit.value;  
     insertionPoint?.appendChild(template.content);
 
 
 });
+var sum = 5;
+function getAverage()
+{
+average.forEach(element => {
+    sum += element;
+    console.log('Element is ' + element)
+    console.log('Sum is ' + sum)
+    average.pop();
+});
+console.log('the count is listed as ' + count)
+var whatever = sum / count;
+console.log('average is listed as ' + whatever)
+return document.getElementById('averageRating')!.innerHTML = whatever.toString();
+}
+function getTemplateClone(templateID:string): HTMLTemplateElement
+{
+    return document.getElementById('responseTemplate')?.cloneNode(true) as HTMLTemplateElement;
+}
+
+
