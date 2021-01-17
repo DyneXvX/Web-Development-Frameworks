4b4a037eb29b7a0f61790482d72137d2b81af07e
Justn Thoms <DyneXvX@outlook.com>
2021-01-17 13:22:49 -0500

    project 1 finished

--- Review Page/dist/index.js
+++ Review Page/dist/index.js
@@ -6,30 +6,41 @@ var userCommit = document.getElementById('commit');
 var template = document.getElementById('responseTemplate');
 var insertionPoint = document.getElementById('responsePoint');
 var count = 1;
-var average = [];
+var averageArray = [];
 btnSubmit.addEventListener('click', function (event) {
+    var _a;
+    if (Validation() == false)
+        return;
     var template = getTemplateClone('responseTemplate');
     template.content.getElementById('userRating').innerHTML = userRating.value;
     count = count + 1;
     var rate = parseInt(userRating.value);
-    average.push(rate);
+    averageArray.push(rate);
     getAverage();
     template.content.getElementById('userName').innerHTML = userName.value;
     template.content.getElementById('userReview').innerText = userCommit.value;
     insertionPoint === null || insertionPoint === void 0 ? void 0 : insertionPoint.appendChild(template.content);
+    //if you check this can you let me know why it refreshes I thought defer stopped that?
+    event.preventDefault();
+    (_a = document.querySelector('form')) === null || _a === void 0 ? void 0 : _a.reset();
 });
+//first review joke
 var sum = 5;
 function getAverage() {
-    average.forEach(function (element) {
+    averageArray.forEach(function (element) {
         sum += element;
-        console.log('Element is ' + element);
-        console.log('Sum is ' + sum);
-        average.pop();
+        averageArray.pop();
     });
-    console.log('the count is listed as ' + count);
-    var whatever = sum / count;
-    console.log('average is listed as ' + whatever);
-    return document.getElementById('averageRating').innerHTML = whatever.toString();
+    var average = sum / count;
+    var roundAverage = Math.round(average * 100) / 100;
+    return document.getElementById('averageRating').innerHTML = roundAverage.toString();
+}
+function Validation() {
+    //required value check        
+    if (userRating.value == "" || userName.value == "" || userCommit.value == "") {
+        return false;
+    }
+    return true;
 }
 function getTemplateClone(templateID) {
     var _a;
--- Review Page/src/index.ts
+++ Review Page/src/index.ts
@@ -8,36 +8,58 @@ var template = document.getElementById('responseTemplate') as HTMLTemplateElemen
 var insertionPoint = document.getElementById('responsePoint');
 
 var count = 1;
-var average: number[] = [];
+var averageArray: number[] = [];
 
-btnSubmit.addEventListener('click', (event)=> {
-    var template = getTemplateClone('responseTemplate');       
-    template.content.getElementById('userRating')!.innerHTML = userRating.value;       
+btnSubmit.addEventListener('click', (event)=> {    
+        if (Validation() == false)       
+            return;
+        
+        
+        var template = getTemplateClone('responseTemplate');       
+        template.content.getElementById('userRating')!.innerHTML = userRating.value;       
+        
+        count = count + 1;
     
-    count = count + 1;      
-    var rate = parseInt(userRating.value);
-    average.push(rate);
-    getAverage();
-    template.content.getElementById('userName')!.innerHTML = userName.value;  
-    template.content.getElementById('userReview')!.innerText = userCommit.value;  
-    insertionPoint?.appendChild(template.content);
+        var rate = parseInt(userRating.value);
+        averageArray.push(rate);
+        getAverage();
+    
+        template.content.getElementById('userName')!.innerHTML = userName.value;  
+        template.content.getElementById('userReview')!.innerText = userCommit.value;  
+        insertionPoint?.appendChild(template.content);
 
-
+        //if you check this can you let me know why it refreshes I thought defer stopped that?
+        event.preventDefault();
+        document.querySelector('form')?.reset();
+        
+        
 });
+//first review joke
 var sum = 5;
+
 function getAverage()
 {
-average.forEach(element => {
-    sum += element;
-    console.log('Element is ' + element)
-    console.log('Sum is ' + sum)
-    average.pop();
-});
-console.log('the count is listed as ' + count)
-var whatever = sum / count;
-console.log('average is listed as ' + whatever)
-return document.getElementById('averageRating')!.innerHTML = whatever.toString();
+    averageArray.forEach(element => {
+        sum += element;
+        averageArray.pop();
+    });
+
+    var average = sum / count;
+    var roundAverage = Math.round(average*100)/100;
+    return document.getElementById('averageRating')!.innerHTML = roundAverage.toString();
 }
+
+function Validation()
+{    
+    //required value check        
+    if(userRating.value == "" || userName.value == "" || userCommit.value == "")
+    {        
+        return false;
+    }     
+    return true;
+    
+}
+
 function getTemplateClone(templateID:string): HTMLTemplateElement
 {
     return document.getElementById('responseTemplate')?.cloneNode(true) as HTMLTemplateElement;
